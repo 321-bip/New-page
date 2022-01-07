bc230a3d84d8661f4b81cf1a52ee11c1d2a1aefd
marcos <dominicmarcos80@gmail.com>
2022-01-07 18:19:03 -0300

    ok

--- scroll.js
+++ scroll.js
@@ -1,9 +1,10 @@
 
 function percorerElemento()
  {
-       const listaElementos = document.querySelectorAll('.link-sessao');
-       listaElementos.forEach((elemento)=>{
-       elemento.addEventListener('click', rolagemDaPagina)
+   const listaElementos = document.querySelectorAll('.link-sessao');
+
+         listaElementos.forEach((elemento) => {
+         elemento.addEventListener('click', rolagemDaPagina)
      })
  }
   
@@ -11,13 +12,24 @@ function percorerElemento()
 function distaciaTop(elemento)
  {
    const id = elemento.getAttribute("href");
-       return document.querySelector(id).offsetTop;
+         return document.querySelector(id).offsetTop;
  }
 
 function rolagemDaPagina(event){
-          event.preventDefault();
-    let topoDoElemento = distaciaTop(event.target) - 80;
-        window.scroll(0,topoDoElemento)
+        event.preventDefault();
+  const topoDoElemento = distaciaTop(event.target) - 80;
+        window.scroll(0,topoDoElemento);
          
 } 
- percorerElemento()
+
+function rolagemSuave()
+ {
+   const tagHtml = document.querySelector('html');
+         tagHtml.style.scrollBehavior = ' smooth';
+ }
+
+function man(){
+   rolagemSuave()
+   percorerElemento();
+}
+man();
