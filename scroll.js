d867f5e73692d45829bcc2914146b52da647b5dc
marcos <dominicmarcos80@gmail.com>
2022-01-06 23:10:05 -0300

    funsao scroll

--- scroll.js
+++ scroll.js
@@ -1,1 +1,23 @@
-// teste branch temp.
+
+function percorerElemento()
+ {
+       const listaElementos = document.querySelectorAll('.link-sessao');
+       listaElementos.forEach((elemento)=>{
+       elemento.addEventListener('click', rolagemDaPagina)
+     })
+ }
+  
+
+function distaciaTop(elemento)
+ {
+   const id = elemento.getAttribute("href");
+       return document.querySelector(id).offsetTop;
+ }
+
+function rolagemDaPagina(event){
+          event.preventDefault();
+    let topoDoElemento = distaciaTop(event.target) - 80;
+        window.scroll(0,topoDoElemento)
+         
+} 
+ percorerElemento()
