74c25fc7828b7e60f22629f9fc3721dc3736acfa
marcos <dominicmarcos80@gmail.com>
2022-01-09 20:57:45 -0300

    linpesa

--- contato.js
+++ contato.js
@@ -1,15 +1,29 @@
 function mensagemUsuario(){
 
-    let nomeContato = document.getElementById("capitura").elements[1].value
-    alert(`${nomeContato} Email Enviado com susesso!!!!`);
+    const nomeContato = document.getElementById("capitura").elements[1].value
+        alert(`${nomeContato} Email Enviado com susesso!!!!`);
 }
-   document.getElementById("area-enviar").addEventListener("click",redireciona)
+  
               
 function redireciona(){
 
-    let link = "https://321-bip.github.io/New-page/"
-    let input4 = document.getElementById("capitura").elements[4]
+    const link = "https://321-bip.github.io/New-page/"
+    const input4 = document.getElementById("capitura").elements[4]
         input4.setAttribute("value",link)
         mensagemUsuario() 
-}   
+}
+
+function eventos()
+ {
+    document.getElementById("area-enviar").addEventListener("click", redireciona)
+ }
+
+ 
+
+function man()
+ {
+    eventos()
+ } 
+
+ man()
     
